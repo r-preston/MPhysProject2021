f459a3a8a27a00a285ea2f7a8b21c1eda5a0d214
r-preston <rowan.t.preston@gmail.com>
2020-10-15 11:10:22 +0100

    findZpeak.cpp produces a histogram peaking as approximately the accepted mass for the Z boson

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-all: example.exe
+all: findZpeak.exe example.exe
 
 %.exe: %.cpp
 	g++ -std=c++17 -Wall -ldl `root-config --cflags` `root-config --libs --glibs` $^ -o $@
--- findZpeak.cpp
+++ findZpeak.cpp
@@ -1,14 +1,15 @@
 #include <TChain.h>
 #include <TCanvas.h>
+#include <TLine.h>
 #include <TH1F.h>
 #include <string>
 #include <iostream>
 
-std::string const DATADIR = "/storage/epp2/phshgg/MPhysProject2021/";
+std::string const DATADIR = "./data/";
 
 int main(){
 
-  TChain ch("WpIso/DecayTree");
+  TChain ch("Z/DecayTree");
 
   std::string const sqrts = "5"/*TeV*/,
     stripping = "32",
@@ -20,19 +21,25 @@ int main(){
   ch.Add(path.c_str());
 
   int const nbins = 100;
-  float const xmin = 15.,
-    xmax = 60.;
+  float const xmin = 10.,
+    xmax = 400.;
 
-  TH1F hist("hist_name","title",nbins,xmin,xmax);
+  TH1F hist("hist_name","Z Boson Mass Fit",nbins,xmin,xmax);
 
-  std::string expression = "1.e-3*mu_PT>>";
+  std::string expression = "1E-3*sqrt(2*((mup_PT*cosh(mup_ETA))*(mum_PT*cosh(mum_ETA))*(1-cos((2*atan(exp(-mup_ETA)))+(2*atan(exp(-mum_ETA)))))))>>";
   expression += hist.GetName();
 
   ch.Draw(expression.c_str());
 
+  TLine l = TLine(91.19,5.,91.19,1900);
+  l.SetLineColor(kRed);
+  l.Draw();
+
   TCanvas canv;
+  canv.Range( xmin, 0., xmax, 150. );
 
   hist.Draw();
+  l.Draw();
 
   canv.SaveAs("plot.pdf");
 
