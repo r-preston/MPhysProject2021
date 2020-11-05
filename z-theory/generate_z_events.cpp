07d0e2e1f3639d5d4a5f7dd997fa1d93adf62b66
Rowan Preston <phuswg@godzilla.csc.warwick.ac.uk>
2020-11-05 14:28:38 +0000

    Working example code and Makefile for Godzilla

--- z-theory/generate_z_events.cpp
+++ z-theory/generate_z_events.cpp
@@ -10,16 +10,18 @@ int main()
   pythia.readString("Beams:eCM = 7000.");
   pythia.init();
 
-  ParticleData particle_data;
+  
+  Event& event = pythia.event;
+  //ParticleData particle_data;
 
   // Begin event loop. Generate event. Skip if error. List first one.
-  for (int iEvent = 0; iEvent < 15; ++iEvent)
+  for (int iEvent = 0; iEvent < 100; ++iEvent)
   {
     if (!pythia.next()) continue;
 
     if(event[iEvent].isFinal() and event[iEvent].isCharged())
     {
-    std::cout << event[iEvent].name() << std::endl;
+      std::cout << event[iEvent].name() << std::endl;
     }
   }
   //pythia.stat();
