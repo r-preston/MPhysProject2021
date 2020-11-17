95963a3bf0c0806af50452b43de57f467ecf5dd7
Laura Cairns <phukjd@godzilla.csc.warwick.ac.uk>
2020-11-17 16:38:28 +0000

    Calculate luminosity and Z cross sectionAA

--- scripts/measure_xsec_lumi.py
+++ scripts/measure_xsec_lumi.py
@@ -5,6 +5,8 @@ import ROOT
 
 file_path = "/storage/epp2/phshgg/DVTuples__v23/5TeV_2017_32_Down_EW.root"
 
+
+#CALCULATE LUMINOSITY
 rfile = ROOT.TFile(file_path)
 lumi_tuple = rfile.Get('GetIntegratedLuminosity/LumiTuple')
 
@@ -15,11 +17,18 @@ for entry in lumi_tuple:
     #print(entry.IntegratedLuminosity, entry.IntegratedLuminosityErr)
     total_lumi += entry.IntegratedLuminosity
     #total_lumi_err_sum += entry.IntegratedLuminosityErr*entry.IntegratedLuminosityErr
-    
-#print(total_lumi)
-#total_lumi_err = total_lumi_err_sum**0.5
-#print(total_lumi_err)
 
+#Calculate error using tuple (not used)
+#total_lumi_err = total_lumi_err_sum**0.5
+    
+#Calculate error as 5%
+total_lumi_err = total_lumi*0.05
+
+print('Integrated Luminosity (pb-1) = {}'.format(total_lumi))
+print('IL 5% error (pb-1) = {}'.format(total_lumi_err))
+
+
+# CALCULATE CROSS SECTION
 ch = ROOT.TChain('Z/DecayTree')
 ch.Add(file_path)
 
@@ -31,8 +40,9 @@ for entry in ch:
        and entry.mup_ETA > 2 and entry.mup_ETA < 4.5 and entry.mum_ETA > 2 and entry.mum_ETA < 4.5):
         count += 1
 
-print('count = {}'.format(count))
 xsec = count/total_lumi
 xsec_err = xsec - (count/(total_lumi*1.05))
-print('xsec = {}'.format(xsec))
-print('xsec error = {}'.format(xsec_err))
+
+print('Z Count = {}'.format(count))
+print('Z Cross Section (pb) = {}'.format(xsec))
+print('XSec Error (pb) = {}'.format(xsec_err))
