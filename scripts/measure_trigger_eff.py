5f65c34743c97a089c257dd14b2751df61bae36c
Laura Cairns <phukjd@godzilla.csc.warwick.ac.uk>
2020-11-19 14:47:11 +0000

    Calculate trigger efficiency, write to file

--- scripts/measure_trigger_eff.py
+++ scripts/measure_trigger_eff.py
@@ -2,6 +2,9 @@
 from ROOT import gROOT
 gROOT.SetBatch(True)
 import ROOT
+import json
+
+data = {}
 
 file_path = "/storage/epp2/phshgg/DVTuples__v23/5TeV_2017_32_Down_EW.root"
 
@@ -16,14 +19,14 @@ both_trigger = float(ch.GetEntries("mup_L0MuonDecision_TOS == 1 && mum_L0MuonDec
 mup_eff = both_trigger/mum_trigger
 mum_eff = both_trigger/mup_trigger
 
-both_eff = 2*both_trigger/(mum_trigger+mup_trigger)
+mup_mum_eff = mup_eff * mum_eff
 
-mup_mum_eff = mup_eff + mum_eff - both_eff
+print("Trigger Efficiency = {}".format(mup_mum_eff))
 
-print(mup_trigger)
-print(mum_trigger)
-print(both_trigger)
-print(mup_eff)
-print(mum_eff)
-print(both_eff)
-print(mup_mum_eff)
+data ={"trigger_efficiency":mup_mum_eff}
+
+with open('data.txt', 'r+') as outfile:
+    data_file = json.load(outfile)
+    data_file.update(data)
+    outfile.seek(0)
+    json.dump(data_file,outfile)
