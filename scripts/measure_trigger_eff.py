96dac283468413ffab5d641fcc5883d79465ab7b
Laura Cairns <phukjd@godzilla.csc.warwick.ac.uk>
2020-11-23 23:52:27 +0000

    Relative uncertainty calculation for trigger efficiency

--- scripts/measure_trigger_eff.py
+++ scripts/measure_trigger_eff.py
@@ -3,8 +3,7 @@ from ROOT import gROOT
 gROOT.SetBatch(True)
 import ROOT
 import json
-
-data = {}
+import math
 
 file_path = "/storage/epp2/phshgg/DVTuples__v23/5TeV_2017_32_Down_EW.root"
 
@@ -14,16 +13,19 @@ ch.Add(file_path)
 mup_trigger = float(ch.GetEntries("mup_L0MuonDecision_TOS == 1"))
 mum_trigger = float(ch.GetEntries("mum_L0MuonDecision_TOS == 1"))
 both_trigger = float(ch.GetEntries("mup_L0MuonDecision_TOS == 1 && mum_L0MuonDecision_TOS == 1"))
-
+N_total = float(ch.GetEntries())
 
 mup_eff = both_trigger/mum_trigger
 mum_eff = both_trigger/mup_trigger
 
 mup_mum_eff = mup_eff + mum_eff - mup_eff * mum_eff
+mup_mum_eff_err = math.sqrt(mup_mum_eff*(1-mup_mum_eff)/N_total)
+
 
 print("Trigger Efficiency = {}".format(mup_mum_eff))
+print("Trigger Efficiency Relative Uncertainty = {}".format(mup_mum_eff_err))
 
-data ={"mup":mup_eff, "mum":mum_eff, "trigger_efficiency":mup_mum_eff}
+data ={"mup":mup_eff, "mum":mum_eff, "trigger_efficiency":mup_mum_eff, "trigger_eff_error":mup_mum_eff_err}
 
 with open('efficiencies.json', 'w') as outfile:
     json.dump(data,outfile)
