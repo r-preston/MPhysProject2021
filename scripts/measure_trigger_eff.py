a66669846851687d47d17e04e5d38a09ed4dab76
Laura Cairns <phukjd@godzilla.csc.warwick.ac.uk>
2020-11-26 17:54:35 +0000

    Updated trigger efficiency to write to Latex

--- scripts/measure_trigger_eff.py
+++ scripts/measure_trigger_eff.py
@@ -4,6 +4,7 @@ gROOT.SetBatch(True)
 import ROOT
 import json
 import math
+import os
 
 file_path = "/storage/epp2/phshgg/DVTuples__v23/5TeV_2017_32_Down_EW.root"
 
@@ -29,3 +30,11 @@ data ={"mup":mup_eff, "mum":mum_eff, "trigger_efficiency":mup_mum_eff, "trigger_
 
 with open('efficiencies.json', 'w') as outfile:
     json.dump(data,outfile)
+
+current_dir = os.getcwd()
+save_path = os.path.join(current_dir, 'doc/measurement_doc/results/')
+
+with open(save_path+'trig_eff_output.tex', 'w') as texfile:
+    texfile.write("Trigger Efficiency = ${:.{prec}f} \pm {:.{prec}f}$\\\\".format(mup_mum_eff, mup_mum_eff*mup_mum_eff_err, prec=4))
+    texfile.write("Trigger Efficiency Relative Uncertainty = ${:.{prec}f}$\\\\".format(mup_mum_eff_err, prec=5))
+
