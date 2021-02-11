164ba77a10c44c0fd8099f91b75a30d5461cd242
Laura Cairns <phukjd@godzilla.csc.warwick.ac.uk>
2021-02-11 01:16:05 +0000

    Update to top level script: Include W analysis

--- scripts/run_xsec_analysis.sh
+++ scripts/run_xsec_analysis.sh
@@ -3,6 +3,11 @@ make
 ./scripts/get_luminosity.py
 ./scripts/measure_trigger_eff.py
 ./scripts/measure_xsec.py
+
+./plot_W_dist.exe
+./W_background.exe
+./scripts/make_W_latex.py
+
 cd doc/measurement_doc/
 pdflatex measurement_report.tex
 gio open measurement_report.pdf
